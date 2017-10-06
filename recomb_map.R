379b641cfff481924a2657e0c1aca66d2e6beaae
ecalfee <erincalfee@gmail.com>
2017-10-05 18:07:09 -0700

    getting Liu 2015 map

---
+++ recomb_map.R
@@ -0,0 +1,12 @@
+# I will be using the observed crosses in haploid drones from Liu 2015 to create a smoothed recombination map 
+# to get estimated genetic positions for physical markers
+
+# Load recombination map from Liu 2015
+r <- read.csv("data/recomb_map/Liu_2015/S5_rates.csv")
+colnames(r) <- c("linkage_group", "pos_Mb", "n_crossovers_per_drone", "rate")
+# there were 43 drones, but the rates per drone must be heavily rounded
+unique(43*r$n_crossovers_per_drone)
+
+r$CO_counts <- as.integer(r$rate/min(r[r$rate != 0, "rate"]))
+sum(r$CO_counts) #2148 not expected 3505 -- are some missing?
+unique(r$linkage_group)
