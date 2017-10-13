2330749c195277dcad196434babd72fc47e5005b
Erin Calfee <erincalfee@gmail.com>
2017-10-13 13:30:20 -0700

    round not integer in recomb rates

--- recomb_map.R
+++ recomb_map.R
@@ -7,6 +7,10 @@ colnames(r) <- c("linkage_group", "pos_Mb", "n_crossovers_per_drone", "rate")
 # there were 43 drones, but the rates per drone must be heavily rounded
 unique(43*r$n_crossovers_per_drone)
 
-r$CO_counts <- as.integer(r$rate/min(r[r$rate != 0, "rate"]))
-sum(r$CO_counts) #2148 not expected 3505 -- are some missing?
+r$CO_counts <- round(r$rate/min(r[r$rate != 0, "rate"]))
+r$CO_counts <- round(r$rate*43/10000) #equivalent. Need to round because of shortened decimal places when file was converted to .csv
+
+
+sum(r$CO_counts) #2177 not expected 3505 -- are some missing?
 unique(r$linkage_group)
+# Emailed authors about the problem.
