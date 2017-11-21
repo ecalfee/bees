670fceb471f47ab77057fee79ef7c83f0af12cb2
ecalfee <erincalfee@gmail.com>
2017-11-20 18:05:42 -0800

    smoothing recomb. rate and running NGSadmix

--- recomb_map.R
+++ recomb_map.R
@@ -1,8 +1,10 @@
+library(rethinking)
+library(MonoPoly)
 # I will be using the observed crosses in haploid drones from Liu 2015 to create a smoothed recombination map 
 # to get estimated genetic positions for physical markers
 
 # Load recombination map from Liu 2015
-r <- read.csv("data/recomb_map/Liu_2015/S5_rates.csv")
+r <- read.csv("data/recomb_map/Liu_2015/S5_rates.csv", stringsAsFactors = FALSE)
 colnames(r) <- c("linkage_group", "pos_Mb", "n_crossovers_per_drone", "rate")
 # there were 43 drones, but the rates per drone must be heavily rounded
 unique(43*r$n_crossovers_per_drone)
@@ -20,6 +22,77 @@ unique(r$linkage_group)
 # calculate posterior
 genome_length = dim(r)[1] #in .01 Mb units, this is the total length included in Liu 2015 (skips gaps)
 mean_r = sum(r$CO_counts)/genome_length
+r$id = 1:length(r$CO_counts) # add an id column for the segment of the genome of interest
+r$lg = as.numeric(as.factor(r$linkage_group))
+
+# mean of gamma = shape*scale (scale = theta = inverse of beta)
+m0.r <- map2stan(
+  alist(
+    CO_counts ~ dpois(mu), # observed crossovers are poisson with mean = mean rate for whole genome
+    mu ~ dgamma(shape = .01, scale = 1) # estimate a mean rate
+    # using a hyperprior to estimate 1 mu doesn't make any sense
+    #k ~ dgamma(shape = .01, sd = 1), # 1 is close to the mean observation; too close to zero to put a normal prior
+    #theta ~ dexp(1)
+  ),
+  data = r,
+  warmup = 1000, iter = 2000, chains = 4, cores = 4, start = list(mu = 0.1, k = 0.1, theta = 1)
+)
+m0LG.r <- map2stan( # can I first get estimates by linkage group
+  alist(
+    CO_counts ~ dpois(lambda = lambda), # observed crossovers are poisson with mean = mean rate for whole genome
+    lambda <- mu[lg],
+    mu[lg] ~ dgamma(shape = k, scale = theta), # estimate a mean rate for each linkage group, with hyperprior over chromosomes
+    k ~ dgamma(shape = 0.1, scale = 1), # 1 is close to the mean observation; too close to zero to put a normal prior
+    theta ~ dexp(1)
+  ),
+  data = r,
+  warmup = 1000, iter = 2000, chains = 4, cores = 4, start = list(mu = rep(1, 16), k = 0.1, theta = 1)
+)
+post0LG.r <- extract.samples( m0LG.r )
+par(mfrow=c(1,1))
+dens(post0LG.r$k)
+dens(post0LG.r$theta)
+apply(post0LG.r$mu, 2, dens)
+
+r_short = as.list(r[1:100,])
+# I thought this is what I wanted to do but it doesn't make a lot of sense --- and would save very large # of chains (21970 of them)
+m1.r <- map2stan(
+  alist(
+    CO_counts ~ dpois(lambda = lambda), # observed crossovers are poisson with mean = mean rate for that local region
+    lambda <- mu[id],
+    mu[id] ~ dgamma(shape = k, scale = theta), ## estimate a mean rate per segment using a hyperprior on the mean # COs
+    k ~ dgamma(shape = 0.1, scale = 1), # 1 is close to the mean observation; too close to zero to put a normal prior
+    theta ~ dexp(1) 
+  ),
+  data = r_short,
+  warmup = 10, iter = 10, chains = 4, cores = 4, start = list(mu = rep(1, 21970), k = 0.1, theta = 1)
+)
+
+
+# THe modeling above doesn't make a lot of sense -- instead I will try to fit a monotone increasing polynomial
+# as a smoothing technique
+r1 = r[r$linkage_group == "LG1",][1:10,]
+r1$posMb = sapply(strsplit(r1$pos_Mb, split = "~"), function(x) as.numeric(x[2])) # get physical Mb position
+r1$posM = cumsum(r1$n_crossovers_per_drone)
+pos_byLG = lapply(unique(r$linkage_group), # for each linkage group (of 16)
+             function(g) sapply(strsplit(r[r$linkage_group == g, "pos_Mb"], split = "~"), # get the end of each interval
+                               function(x) as.numeric(x[2]))) # as a number
+r_byLG = lapply(unique(r$linkage_group),
+                function(g) cumsum(r[r$linkage_group == g, "n_crossovers_per_drone"]))
+par(mfrow=c(2,2))
+lapply(1:16, function(i) plot(pos_byLG[[i]], r_byLG[[i]], 
+                              xlab = "position (omit gaps)",
+                              ylab = "r (Morgans)",
+                              main = unique(r$linkage_group)[i]))
+#approxfun
+#monopoly -smooths over larger windows
+d = data.frame(list(pos_byLG = pos_byLG[[15]], r_byLG = r_byLG[[15]]))
+par(mfrow=c(1,1))
+m1 = monpol(r_byLG ~ pos_byLG, data = d,
+       degree = 9, plot.it = T) # will need to plot on my own just final fit (not all)
+
+
+
 #dpois(x = unique(r$CO_counts), lambda = mean_r)
 plot(unique(r$CO_counts), 
      log(dpois(x = unique(r$CO_counts), lambda = mean_r)),
