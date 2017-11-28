77cd60bd57418313e6e0143865885fff71409a9c
ecalfee <erincalfee@gmail.com>
2017-11-27 18:26:59 -0800

    admixture plot for pres

--- visualize_bee_admixture.R
+++ visualize_bee_admixture.R
@@ -29,11 +29,24 @@ admix[, "pop"] = ifelse(!is.na(admix$pop), admix$pop,
                         ifelse(!is.na(admix$label_s), 
                                substring(admix$label_s, 1, 1), 
                                         "A"))
+admix$pop = ifelse(is.na(admix$year), admix$pop, paste0(admix$pop, "_", admix$year))
+admix$pop = ifelse(admix$pop == "S", "A", admix$pop)
+admix$ref = admix$pop %in% c("C", "A", "M")
 #write.csv(x = admix[,c("id", "pop", "year", "study")], quote = F, file = "Bee_bam_include_geno_AC_12_metadata.txt")
 admix = arrange(admix, year) %>% # organize by population and within population, by year
-  arrange(., pop)
-barplot(t(as.matrix(admix[,c("A", "C", "M")])),col=c("red", "green", "blue"),space=0,border=NA,xlab="Individuals",ylab="admixture")
-text(tapply(1:length(admix$pop),admix$pop,mean),-0.05,unique(admix$pop),xpd=T)
+  arrange(., pop) %>%
+  arrange(., ref)
+pops_to_plot = c("A", "M", "C", "Davis_2014", "Stebbins_2014", "Riverside_1999", "SanPedro_2002", "Placerita_2014", "Riverside_2014")
+admix2 = admix[admix$pop %in% pops_to_plot, ]
+admix2$pop = factor(admix2$pop, levels = pops_to_plot, ordered = T)
+admix2 = admix2 %>% arrange(., pop)
+barplot(t(as.matrix(admix2[, c("A", "M", "C")])),
+        col=c("red", "darkblue", "lightblue"),
+        space=0,border=NA,
+        xlab="Individuals",
+        ylab="admixture")
+#labels aren't spaced correctly (last ind. with A is from Stebbins)
+text(tapply(1:length(admix2$pop),admix2$pop,mean),-0.15,unique(admix2$pop),xpd=T, srt = -45)
 # I accidentally included a population from Yemen in the Harpur data (O lineage - in #7 in 8th row)
 admix[,c("pop")]
 # I can repeat this with just 2 populations to confirm A and C/M stand out (check sensitivity)
