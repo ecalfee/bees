3535a6ee2b2a77c477dba1402ad144dc6cc370f3
ecalfee <erincalfee@gmail.com>
2018-06-28 18:12:26 -0700

    added negative binomial results for simulations across all individuals -- looks good

--- sims_downsample/compare_local_ancestry.R
+++ sims_downsample/compare_local_ancestry.R
@@ -6,75 +6,111 @@ library(dplyr)
 # sequence data with inferred genotypes to those inferred from
 # low coverage data (simulated binomial sampling of alleles -> reads)
 
-id = "SRCD47A"
-id = "SRCD10B"
-id = "SRCD9A"
+#id = "SRCD47A"
+#id = "SRCD10B"
+#id = "SRCD9A"
+id = "Riv2014"
+ids = paste0("SRCD", c("6A", "9A", "10B", "47A", "49A", "51B", "51C", "54C"))
 # high coverage 'true ancestry'
-d = read.table(paste0("../data/sims_downsample/ancestry_hmm/est_t2/",
-id, ".posterior"), 
-               stringsAsFactors = F, header = T)
+d = do.call(rbind, lapply(ids, function(id) 
+  read.table(paste0("../data/sims_downsample/ancestry_hmm/est_t2/", 
+                   id, ".posterior"), stringsAsFactors = F, header = T)))
 
 s = 1001
 time = "fix_t" #est_t2
 colnames(d)[3:8] 
-ancTypes = c("AA", "AC", "AM", "CC", "CM", "MM") 
-continents = c("AfrAfr", "AfrEu", "AfrEu", "EuEu", "EuEu", "EuEu")
+ancTypes = c("AA", "AC", "AM", "CC", "CM", "MM")
+continents = c("AfrAfr", "AfrEu", "EuEu")
+continents_long = c("AfrAfr", "AfrEu", "AfrEu", "EuEu", "EuEu", "EuEu")
 postH = .9 # posterior minimum for high confidence ancestry calls
 postT = .9 # posterior to call a true call
 postF = .5 # posterior to call a false call
-# 70% genome is called confidently for ancestry
+dir_prefix = "../data/sims_downsample/ancestry_hmm/wSeqError/"
+# ~72% genome is called confidently for ancestry
 sum(d$X2.0.0>=postH | d$X1.1.0>=postH |
       d$X0.2.0>=postH | d$X0.1.1>=postH |
       d$X0.0.2>=postH | d$X1.0.1>=postH)/nrow(d)
-
+# functions
+# calculate power to recover in low coverage data ancestry for positions with posteriors over postH in high-cov. data
 calcPwr = function(dTrue, dSim, ancN){
   colN = ancN+2
   # true positives
   tp = sum(dSim[dTrue[ , colN] >= postH, colN] >= postT)/sum(dTrue[ , colN]>=postH)
   # false positives
-  fp = (sum(apply(dSim[dTrue[ , colN]>=postH, 3:8], 1, max) >= postF) - sum(dSim[dTrue[ , colN]>=postH, colN] >= postF))/sum(dTrue[ , colN]>=postH)
+  fp = (sum(apply(dSim[dTrue[ , colN]>=postH, 3:ncol(dSim)], 1, max) >= postF) - sum(dSim[dTrue[ , colN]>=postH, colN] >= postF))/sum(dTrue[ , colN]>=postH)
   return(c(tp=tp, fp=fp))  
 }
-poisX = lapply(1:10, function(x)
-  read.table(paste0("../data/sims_downsample/ancestry_hmm/", x, 
-                                       "x/", "poisson", "/seed", s, "/", time, "/", id, ".posterior"), 
-                                stringsAsFactors = F, header = T))
+# convert population level ancestry posteriors to continent level ancestry posteriors:
+ACM_to_continent = function(posteriors){
+  post_continent = mutate(posteriors, AfrAfr = X2.0.0) %>%
+    mutate(., AfrEu = X1.1.0 + X1.0.1) %>%
+    mutate(., EuEu = X0.2.0 + X0.1.1 + X0.0.2) %>%
+    select(., chrom, position, AfrAfr, AfrEu, EuEu)
+  return(post_continent)
+}
 
-poisPwr = lapply(poisX, function(pois) sapply(1:6, function(i) 
-  calcPwr(dSim = pois, dTrue = d, ancN = i)))
+poisX = lapply(1:10, function(x)
+  do.call(rbind, lapply(ids, function(id) 
+    read.table(paste0(dir_prefix, x, 
+                      "x/", "poisson", "/seed", s, "/", time, "/", id, ".posterior"), 
+               stringsAsFactors = F, header = T))))
+
+poisPwr = lapply(poisX, function(x) sapply(1:6, function(i) 
+  calcPwr(dSim = x, dTrue = d, ancN = i)))
 
 # calculate power for fixed coverage results
 fixedX = lapply(1:10, function(x)
-  read.table(paste0("../data/sims_downsample/ancestry_hmm/", x, 
-                    "x/", "fixed", "/seed", s, "/", time, "/", id, ".posterior"), 
-             stringsAsFactors = F, header = T))
+  do.call(rbind, lapply(ids, function(id) 
+    read.table(paste0(dir_prefix, x, 
+                      "x/", "fixed", "/seed", s, "/", time, "/", id, ".posterior"), 
+               stringsAsFactors = F, header = T))))
 
-fixedPwr = lapply(fixedX, function(fixed) sapply(1:6, function(i) 
-  calcPwr(dSim = fixed, dTrue = d, ancN = i)))
+fixedPwr = lapply(fixedX, function(x) sapply(1:6, function(i) 
+  calcPwr(dSim = x, dTrue = d, ancN = i)))
+
+# calculate power for negative binomial coverage results
+negbinX = lapply(1:10, function(x)
+  do.call(rbind, lapply(ids, function(id) 
+    read.table(paste0(dir_prefix, x, 
+                      "x/", "negbinom", "/seed", s, "/", time, "/", id, ".posterior"), 
+               stringsAsFactors = F, header = T))))
+
+negbinPwr = lapply(negbinX, function(x) sapply(1:6, function(i) 
+  calcPwr(dSim = x, dTrue = d, ancN = i)))
+
 
 # plot & save results for poisson simulation
-png(paste0("../plots/sim_downsample_pwr_", id, ".png"),
-     height = 6, width = 8, units = "in", res = 120)
+png(paste0("../plots/sims_downsample/wSeqError/pwr_ACM_", id, ".png"),
+     height = 6, width = 10, units = "in", res = 150)
 plot( # make empty plot frame
     1:10, rep(0, 10), col = NULL,
     ylim = c(0,1), ylab = paste0("Proportion correct ancestry (post>", postT,")"),
     xlab = paste0("simulated coverage"),
     main = paste0("Proportion high conf. (post>", postH, ") ancestry calls replicated in low-coverage"),
     sub = paste("False-positive rate (post>", postF, ") for incorrect ancestry plotted at bottom") 
-) # add lines 
+) 
+# add lines for poisson 
 for (i in 1:6){
   lines(1:10, sapply(1:10, function(x) poisPwr[[x]][1, i]), 
         col = rainbow(6)[i], lwd = 2) 
 }
-legend("right", legend = ancTypes, col = rainbow(6), pch = 20, title = "True Ancestry")
 
 # add lines for fixed coverage
 for (i in 1:6){
   lines(1:10, sapply(1:10, function(x) fixedPwr[[x]][1, i]), 
         col = rainbow(6)[i], lwd = 2, lty = 2) 
 }
-legend("bottomright", legend = c("Poisson", "Fixed at mean"), 
-       title = "Coverage distribution", lwd = 2, lty = c(1, 2), col = "black")
+
+# add lines for neg. binomial
+for (i in 1:6){
+  lines(1:10, sapply(1:10, function(x) negbinPwr[[x]][1, i]), 
+        col = rainbow(6)[i], lwd = 2, lty = 3) 
+}
+
+# add legend
+legend("right", legend = ancTypes, col = rainbow(6), pch = 20, title = "True Ancestry")
+legend("bottomright", legend = c("Poisson", "Fixed at mean", "Neg. Binom."), 
+       title = "Coverage distribution", lwd = 2, lty = c(1, 2, 3), col = "black")
 
 # optional - add lines for false positives
 for (i in 1:6){
@@ -85,7 +121,126 @@ for (i in 1:6){
   lines(1:10, sapply(1:10, function(x) fixedPwr[[x]][2, i]), 
         col = rainbow(6)[i], lwd = 1, lty = 2) 
 }
+for (i in 1:6){
+  lines(1:10, sapply(1:10, function(x) negbinPwr[[x]][2, i]), 
+        col = rainbow(6)[i], lwd = 1, lty = 3) 
+}
 dev.off()
+
+# now a similar analysis at the continent (Afr-Eu) level:
+# 'true ancestry' coded for continent-level analysis (groups C & M together as 'European')
+dCont = ACM_to_continent(d)
+
+c_poisX = lapply(poisX, ACM_to_continent)
+
+c_poisPwr = lapply(c_poisX, function(x) sapply(1:3, function(i) 
+  calcPwr(dSim = x, dTrue = dCont, ancN = i)))
+
+# calculate power for fixed coverage results
+c_fixedX = lapply(fixedX, ACM_to_continent)
+
+c_fixedPwr = lapply(c_fixedX, function(x) sapply(1:3, function(i) 
+  calcPwr(dSim = x, dTrue = dCont, ancN = i)))
+
+# calculate power for negative binomial coverage results
+c_negbinX = lapply(negbinX, ACM_to_continent)
+
+c_negbinPwr = lapply(c_negbinX, function(x) sapply(1:3, function(i) 
+  calcPwr(dSim = x, dTrue = dCont, ancN = i)))
+
+# plot resulting power for continent-level:
+png(paste0("../plots/sims_downsample/wSeqError/pwr_AfrEu_", id, ".png"),
+    height = 6, width = 10, units = "in", res = 150)
+plot( # make empty plot frame
+  1:10, rep(0, 10), col = NULL,
+  ylim = c(0,1), ylab = paste0("Proportion correct ancestry (post>", postT,")"),
+  xlab = paste0("simulated coverage"),
+  main = paste0("Proportion high conf. (post>", postH, ") ancestry calls replicated in low-coverage"),
+  sub = paste("False-positive rate (post>", postF, ") for incorrect ancestry plotted at bottom") 
+) 
+# add lines for poisson 
+for (i in 1:3){
+  lines(1:10, sapply(1:10, function(x) c_poisPwr[[x]][1, i]), 
+        col = rainbow(3)[i], lwd = 2) 
+}
+
+# add lines for fixed coverage
+for (i in 1:3){
+  lines(1:10, sapply(1:10, function(x) c_fixedPwr[[x]][1, i]), 
+        col = rainbow(3)[i], lwd = 2, lty = 2) 
+}
+
+# add lines for neg. binomial
+for (i in 1:3){
+  lines(1:10, sapply(1:10, function(x) c_negbinPwr[[x]][1, i]), 
+        col = rainbow(3)[i], lwd = 2, lty = 3) 
+}
+
+# add legend
+legend("right", legend = continents, col = rainbow(3), pch = 20, title = "True Ancestry")
+legend("bottomright", legend = c("Poisson", "Fixed at mean", "Neg. Binom."), 
+       title = "Coverage distribution", lwd = 2, lty = c(1, 2, 3), col = "black")
+
+# optional - add lines for false positives
+for (i in 1:3){
+  lines(1:10, sapply(1:10, function(x) c_poisPwr[[x]][2, i]), 
+        col = rainbow(3)[i], lwd = 1) 
+}
+for (i in 1:3){
+  lines(1:10, sapply(1:10, function(x) c_fixedPwr[[x]][2, i]), 
+        col = rainbow(3)[i], lwd = 1, lty = 2) 
+}
+for (i in 1:3){
+  lines(1:10, sapply(1:10, function(x) c_negbinPwr[[x]][2, i]), 
+        col = rainbow(3)[i], lwd = 1, lty = 3) 
+}
+dev.off()
+
+
+# just negative binomial (for higher readability):
+# plot resulting power for continent-level:
+png(paste0("../plots/sims_downsample/wSeqError/pwr_negbin_", id, ".png"),
+    height = 6, width = 10, units = "in", res = 150)
+plot( # make empty plot frame
+  1:10, rep(0, 10), col = NULL,
+  ylim = c(0,1), ylab = paste0("Proportion correct ancestry (post>", postT,")"),
+  xlab = paste0("simulated coverage"),
+  main = paste0("Proportion high conf. (post>", postH, ") ancestry calls replicated w/ negBin low-coverage"),
+  sub = paste("False-positive rate (post>", postF, ") for incorrect ancestry plotted at bottom") 
+) 
+
+# add lines for neg. binomial - continent level
+for (i in 1:3){
+  lines(1:10, sapply(1:10, function(x) c_negbinPwr[[x]][1, i]), 
+        col = rainbow(9)[i+6], lwd = 2, lty = 1) 
+}
+
+# add lines for neg. binomial - ancestry level
+for (i in 1:6){
+  lines(1:10, sapply(1:10, function(x) negbinPwr[[x]][1, i]), 
+        col = rainbow(9)[i], lwd = 2, lty = 2) 
+}
+
+# add lines for false positives
+for (i in 1:3){
+  lines(1:10, sapply(1:10, function(x) c_negbinPwr[[x]][2, i]), 
+        col = rainbow(9)[i+6], lwd = 1, lty = 3) 
+}
+for (i in 1:6){
+  lines(1:10, sapply(1:10, function(x) negbinPwr[[x]][2, i]), 
+        col = rainbow(9)[i], lwd = 1, lty = 3) 
+}
+
+
+# add legend
+legend("right", legend = c(ancTypes, continents, "(false pos.)"), col = c(rainbow(9), "black"), pch = 20, title = "True Ancestry",
+       lty = c(rep(2, 6), rep(1, 3), 3))
+
+
+dev.off()
+
+
+
 
 
 # make 'confusion histogram' for specific x coverage
@@ -107,9 +262,9 @@ conf4x[conf4x$posterior >= .001,] %>% # only plot over .001 posterior because to
 # identify true and predicted continents of Ancestry
 # i.e. African or European homozygous or heterozygous
 conf4x$continentPred = sapply(conf4x$ancestryPred, function(anc)
-  continents[which(ancTypes == anc)])
+  continents_long[which(ancTypes == anc)])
 conf4x$continentTrue = sapply(conf4x$ancestryTrue, function(anc)
-  continents[which(ancTypes == anc)])
+  continents_long[which(ancTypes == anc)])
 # group data by continent-level ancestry, summing across ancestry within continental groupings
 conf4x_continent = group_by(conf4x, continentTrue, continentPred, chrom, position) %>%
   summarise(., contPost = sum(posterior))
