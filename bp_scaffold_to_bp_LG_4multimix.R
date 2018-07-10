bb27d3e792b6b5d88fbd25ff369c14e5d5ecded6
ecalfee <erincalfee@gmail.com>
2018-07-10 12:54:14 -0700

    corrected scripts to make all input files for multimix

--- bp_scaffold_to_bp_LG_4multimix.R
+++ bp_scaffold_to_bp_LG_4multimix.R
@@ -4,57 +4,48 @@
 # and converts them to (1) physical bp positions 
 # relative to the whole chromosome,
 # assuming 50,000 bp gaps between scaffolds
-# (2) local recombination rate at that position
-# and (3) recombination positions in cM relative to the beginning of the chromosome
 
-# note: will produce error if more than one chromosome is included in input file
-
+# note: run one chromosome at a time (split bim file in plink by chromosome first)
 
 library(dplyr)
-#library(ggplot2)
 
-# to run: Rscript bp_to_r_Wallberg2015_4multimix.R data/bees_new_positions/lowLDA4/A.frq.counts
+# to run: Rscript bp_scaffold_to_bp_LG_4multimix.R data/bees_new_positions/ALL_splitbychr/ALL_chr1 1 data/sims_downsample/multimix
 # the result is a set of map files for input to MULTIMIX
-# OUTPUT is split by chromosome and saved in OUT_DIR/chr*.map files where * is the chromosome
+# OUTPUT is saved in $out/input/haplotypes/legend_files/chr*.legend files where * is the chromosome
 
 # user input:
 args <- commandArgs(trailingOnly=TRUE)
-POS_FILE = args[1]
-OUT_DIR = args[2]
+BIM_FILE = args[1] # looks for a prefix.bim file made by plink
+CHR = args[2]
+OUT_DIR = args[3]
 
-# start of new chromosome/Linkage Group default distance (in cM; will be divided by 100 to convert to morgans) 
-# this number cannot be negative but is otherwise ignored by ancestry_hmm; 1 Morgan is recommended value
-startingBP = 100
+# create output directory
+if (!dir.exists(paste0(OUT_DIR, "/input/haplotypes/legend_files"))){
+  dir.create(paste0(OUT_DIR, "/input/haplotypes/legend_files"), recursive=TRUE)
+}
 
-# Wallberg map with resolution 100kb scale -- note: blanks are NAs, no recomb. rate estimated for that window
-rmap = read.csv("data/recomb_map/Wallberg_2015/recombination_rates/A.rates.1000.201.low_penalty.csv.cM_Mb.windows.100000.csv",
-                stringsAsFactors = F, header = F, na.strings = "", sep = "\t")
-colnames(rmap) = c("LG", "pos", "window", "cM_Mb")
-# This map has blanks (now NAs) where recombination rates cannot be estimated due to lack of SNPs or gaps (e.g. between scaffolds)
-# for these, I impute a chromosome-level average recombination rate
-# calculate from the average rates across all windows for each Linkage Group
-meanLG = tapply(rmap$cM_Mb, rmap$LG, function(r) mean(r, na.rm = T))
-for (lg in paste0("Group", 1:16)){
-  rmap[rmap$LG==lg & is.na(rmap$cM_Mb), "cM_Mb"] <- meanLG[lg]
-} 
-
-# get position file
-d0 = read.table(POS_FILE, stringsAsFactors = F, header = T)
+# get relative positions (bim file)
+d0 = read.table(paste0(BIM_FILE, ".bim"), stringsAsFactors = F, header = F)
+colnames(d0) <- c("CHR", "SNP_NAME", "R_POS_FALSE", "BP_POS_FALSE", "A1", "A2")
+if (d0$CHR[1] != CHR){
+  stop("error: chromosome in bim file doesn't match chromosome in input argument; use plink to subset bim file to chromosome of interest")
+}
 # reformat to access SNP position information
-SNPs = data.frame(t(sapply(d0$SNP, 
+SNPs = data.frame(t(sapply(d0$SNP_NAME, 
                              function(i) 
                                unlist(strsplit(i, split = "[.]")))), stringsAsFactors = F)
 colnames(SNPs) = c("snpN", "LG", "scaffold", "scaff_pos")
+rownames(SNPs) = NULL 
 # script only accepts one chromosome at a time
 if (length(unique(SNPs$LG)) > 1){
-  stop("error: please subset input data file to only include one LG/chromosome")
+  stop("error: please subset input data file in plink first to only include one LG/chromosome")
 }
 
 # convert characters to integers
 class(SNPs$scaffold) = "integer"
 class(SNPs$scaff_pos) = "integer"
-rownames(SNPs) = NULL 
-d1 = cbind(d0, SNPs)
+
+d1 = cbind(d0[ , c("SNP_NAME", "A1", "A2")], SNPs)
 # find position relative to chromosome, not scaffold, where we assume there is a 50,000 bp gap between any adjacent scaffolds
 # first get absolute start positions for each scaffold
 starts <- read.csv("data/honeybee_genome/Amel_4.5_scaffold_start_positions.csv", stringsAsFactors = F)
@@ -65,48 +56,8 @@ d = left_join(d1, starts[ , c("LG", "scaffold", "pos_start")],
               by = c("LG", "scaffold")) %>%
   mutate(chr_pos = scaff_pos + pos_start)
 
-# create helper function to calculate recombination distance between 2 positions or return 1 if across chromosomes
-rBetween = function(pos1, pos2, lg1, lg2, rmap){
-  if (lg1 != lg2){
-    dist = startingBP # start of new chromosome/Linkage Group 
-  }else{ # within-chromosome
-    if (pos1 > pos2){# error; position 1 is greater than position 2
-      stop(paste("out of order --", lg1, pos1, "seen before", lg2, pos2))
-    }else{
-      # only consider recomb. map for current linkage group
-      rmapLG = rmap[rmap$LG==lg1, ]
-      # index of start of window for position 1 and 2
-      i1 = max(which(rmapLG$pos <= pos1))
-      i2 = max(which(rmapLG$pos <= pos2))
-      if (i1==i2){
-        # within same recombination window, calculate cM distance between
-        dist = (pos2 - pos1) * rmapLG[i1, "cM_Mb"] / 1000000 # division to convert bp to Mbp
-      } else{ # otherwise, solve by recursion
-        # add distance to end of pos 1's window to remaining distance between positions
-        dist = (rmapLG[i1 + 1, "pos"] - pos1) * rmapLG[i1, "cM_Mb"] / 1000000 + 
-          rBetween(pos1 = rmapLG[i1 + 1, "pos"], pos2 = pos2, 
-                             lg1 = lg1, lg2 = lg2, rmap = rmap)
-      }
-    }
-  }
-return(dist)
-}
-# test cases - all good
-#rBetween(pos1 = 1, pos2 = 1, lg1 = "Group1", lg2 = "Group2", rmap = rmap) == -9
-#rBetween(pos1 = 1, pos2 = 1, lg1 = "Group1", lg2 = "Group1", rmap = rmap) == 0
-#rBetween(pos1 = 100, pos2 = 99, lg1 = "Group1", lg2 = "Group1", rmap = rmap) # error
-#rBetween(pos1 = 1, pos2 = 10001, lg1 = "Group1", lg2 = "Group1", rmap = rmap) # .418
-#rBetween(pos1 = 1, pos2 = 100001, lg1 = "Group1", lg2 = "Group1", rmap = rmap) # 4.18
-#rBetween(pos1 = 1, pos2 = 350001, lg1 = "Group1", lg2 = "Group1", rmap = rmap) # 13.16 = 4.18+4.24+3.46+2.56/2
-
-# apply function to calculate recombination distance
-# /100 so distance is converted from cM to Morgans as specified by ancestry_hmm
-d$rDist = c(startingBP, sapply(2:length(d$chr_pos), function(i) 
-  rBetween(pos1 = d[i-1, "chr_pos"], pos2 = d[i, "chr_pos"],
-           lg1 = d[i-1, "LG"], lg2 = d[i, "LG"], rmap = rmap)))/100
-
-# write final output SNP positions file for use by ancestry_hmm
-write.table(d[, c("LG", "chr_pos", "rDist")], file = paste0(POS_FILE, ".rmap"),
-            quote = F, row.names = F, col.names = F)
+# write final output SNP positions file for use by multimix
+write.table(d[, c("SNP_NAME", "chr_pos", "A1", "A2")], file = paste0(OUT_DIR, "/input/haplotypes/legend_files/chr", CHR, ".legend"),
+            quote = F, row.names = F, col.names = F, sep = " ")
 
 
--- sims_downsample/make_multimix_input_geno.sh
+++ sims_downsample/make_multimix_input_geno.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# to run: bees/sims_downsample$ type=admix bim=../data/bees_new_positions/ALL pop=pop1 out=../data/sims_downsample/multimix/input ./make_multimix_input_geno.sh
+# to run: bees/sims_downsample$ type=admix bim=../data/bees_new_positions/ALL chr=16 pop=pop1 out=../data/sims_downsample/multimix/input ./make_multimix_input_geno.sh
+# note: this script counts the A1 allele, not A2 (A2 appears to be the major allele in the plink files)
 echo "making multimix input genotype file."
 echo "population: "$pop
 echo "bim file: "$bim
+echo "chromosome: "$chr
 echo "type of output: "$type
 echo "to output directory: "$out
 # check for input errors
@@ -23,7 +25,7 @@ then
 fi
 
 # make temporary directory
-mkdir -p $out/temp/$pop
+mkdir -p $out/temp_$pop"_"chr$chr/
 # get list of all individual IDs to process
 # first make an individuals file if it doesn't yet exits:
 # with population ID, individual ID, then individual ID again in 3rd column
@@ -34,13 +36,14 @@ else
     echo "file already exists, won't rewrite: "$bim.ind
 fi
 
-IDs=$(awk -v pop=$pop '$1 == $pop {print $2}' $bim.ind)
-CHROMOSOMES={1..16}
+IDs=$(awk -v pop=$pop '$1 == pop {print $2}' $bim.ind)
+echo "IDs to include: " $IDs
+
 
 # get individual counts for all SNPs in bim file; write to temp directory $id.frq.counts
 for id in $IDs; \
-do plink --bfile $bim --freq counts --keep-allele-order --filter individuals.txt $ind \
---out $out/temp/$pop/$id; \
+do plink --bfile $bim --freq counts --keep-allele-order --filter $bim.ind $id \
+--out $out/temp_$pop"_"chr$chr/$id; \
 done
 
 
@@ -50,35 +53,36 @@ if [ $type == "ref" ]
 then
     # make output directory
     mkdir -p $out/input/haplotypes/$pop
+    # save IDs in order
+    echo $IDs > $out/input/haplotypes/$pop/ref.ind;
     # make each individual's output
-    for chr in $CHROMOSOMES;
-    do for id in $IDs;
+    for id in $IDs;
     # counts of A1 haplotype (column 5)
-    do awk -v chr=$chr '$1 == $chr { if ($7 == 0) print $5; else print 9 }' > $out/temp/$pop/$id.chr$chr.strp;
-    echo $id >> $out/input/haplotypes/$pop/ref.ind; # save IDs in order
+    do awk '$1 != "CHR" { if ($7 == 0) print $5; else print 9 }' $out/temp_$pop"_"chr$chr/$id.frq.counts > $out/temp_$pop"_"chr$chr/$id.strp;
     done
     # paste it all together
-    paste -d" " $(for id in $IDs; do ls $out/temp/$pop/$id.chr$chr.strp; done) > $out/input/haplotypes/$pop/chr$chr.genos
+    paste -d" " $(for id in $IDs; do ls $out/temp_$pop"_"chr$chr/$id.strp; done) > $out/input/haplotypes/$pop/chr$chr.genos
 fi
 
 if [ $type == "admix" ]
 then
     # make output directory admixed samples
     mkdir -p $out/input/Samples/genos/$pop
+    # save IDs in order
+    echo $IDs > $out/input/Samples/genos/$pop/admix.ind;
     # make each individual's output
-    for chr in $CHROMOSOMES;
-    do for id in $IDs;
+    for id in $IDs;
     # counts of A1 haplotype (column 5)
-    do awk -v chr=$chr '$1 == $chr { if ($7 > 0) print 9 9 9; elif ($5 == 0) print 0 0 1; elif ($5 == 1) print 0 1 0; else print 0 0 1}' > $out/temp/$pop/$id.chr$chr.strp;
-    echo $id >> $out/input/haplotypes/$pop/ref.ind; # save IDs in order
+    do awk '$1 != "CHR" { if ($7 > 0) print "9 9 9"; else if ($5 == 0) print "0 0 1"; else if ($5 == 1) print "0 1 0"; else print "1 0 0"}' \
+$out/temp_$pop"_"chr$chr/$id.frq.counts > $out/temp_$pop"_"chr$chr/$id.strp;
     done
 
     # make SNP ID output
     # get this part from bp_scaffold_to_bp_LG_4multimix.R for absolute bp positions w/ 50kb gaps
     # paste it all together
-    paste -d" " $out/input/haplotypes/legend_files/chr$chr.legend $(for id in $IDs; do ls $out/temp/$pop/$id.chr$chr.strp; done) > $out/input/Samples/genos/$pop/genos_chr$chr.genos
+    paste -d" " $out/input/haplotypes/legend_files/chr$chr.legend $(for id in $IDs; do ls $out/temp_$pop"_"chr$chr/$id.strp; done) > $out/input/Samples/genos/$pop/genos_chr$chr
 fi
 
 # remove temporary directory
-rm -r $out/temp/$pop
+rm -r $out/temp_$pop"_"chr$chr
 
