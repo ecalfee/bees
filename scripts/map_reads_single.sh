087b081a50d20e2c6bd22e3b8c1e7d804b9f83bb
Erin Calfee <erincalfee@gmail.com>
2018-12-20 20:31:58 -0800

    fixed small typo in directory

--- scripts/map_reads_single.sh
+++ scripts/map_reads_single.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# this script generalizes input for fastq_prefix so it works with the downloaded ncbi data too
+# this script maps reads that are single reads (no pairs), e.g. reference bees downloaded from NCBI
 
 # A script to do raw read mapping/alignment to the honeybee reference genome
-# e.g. to run one sample: scripts$ ./map_reads.sh AR0302 C202SC18101772
+# e.g. to run one sample: scripts$ ./map_reads_single.sh SRR957082 Harpur_2014_NCBI
 # or using nohup and gnu parallel to run many samples:
 #scripts$ nohup parallel --noswap --joblog ../logs/map_reads_Harpur_2014_NCBI.log --jobs 4 \
-#./map_reads_ncbi.sh {1} Harpur_2014_NCBI :::: ../data/Harpur_2014_NCBI/samples.list \
+#./map_reads_single.sh {1} Harpur_2014_NCBI :::: ../data/Harpur_2014_NCBI/samples.list \
 # &> ../logs/map_reads_Harpur_2014_NCBI.out &
 
 # general bash script settings to make sure if any errors in the pipeline fail
@@ -20,24 +20,24 @@ set -o nounset
 # command line arguments:
 ID=$1 # bee ID
 SEQ_RUN=$2 # identifier for sequenced group
-#FASTQ_PREFIX="novo_seq/"${2}"/raw_data/"${ID}"/"${ID}"_"
-FASTQ_PREFIX=$3
-OUTPUT_PREFIX=$4
+FASTQ_PREFIX=${SEQ_RUN}"/fastq_files/"${1}
+DIR_OUT=${SEQ_RUN}"/bam_files"
 
 # move from scripts to data directory
 cd ../data
 
-mkdir -p novo_seq/bam_files
-echo ${PWD} # print current working directory
+mkdir -p ${DIR_OUT}
+
+echo "working directory:"${PWD} # print current working directory
+echo "output directory:"${DIR_OUT}
 
 echo "mapping reads with bowtie2"
 
 bowtie2 --seed 2014 --very-sensitive-local --local \
 -x honeybee_genome/honeybee_Amel_4.5 \
--1 ${FASTQ_PREFIX}*_1.fq.gz \
--2 ${FASTQ_PREFIX}*_2.fq.gz \
+-U ${FASTQ_PREFIX}*_1.fq.gz \
 --rg-id ${SEQ_RUN} --rg SM:${ID} | \
-samtools view -b - > novo_seq/bam_files/${ID}.bam
+samtools view -b - > ${DIR_OUT}/${ID}.bam
 
 echo "alignment done!"
 
@@ -45,6 +45,6 @@ echo "alignment done!"
 # choosing very sensitive local alignment (i.e. reads may be soft-clipped to map)
 # random seed is used to choose a mapping location when there are ties
 # -x is for indexed reference genome
-# -1 and -2 are for paired reads; -U would be used for unpaired reads
+# -U is for unpaired reads
 # converting sam directly to bam using samtools
 # --rg-id and --rg create read group headers required by GATK downstream analysis
