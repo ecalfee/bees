74d0eedd03c8e130978fdf8bdeb6debd418a469e
ecalfee <erincalfee@gmail.com>
2019-02-13 00:20:34 -0800

    now running acgt counts -- got rid of pipefail and it used arguments properly

--- local_ancestry/countReadsACGT.sh
+++ local_ancestry/countReadsACGT.sh
@@ -5,20 +5,17 @@ echo "argument 2 is: $2"
 
 # general bash script settings to make sure if any errors in the pipeline fail
 # then it’s a ‘fail’ and it passes all errors to exit and allows no unset variables
-set –o pipefail
-set –o errexit
-set –o nounset
 
 # this script takes in a sites file and outputs an angsd counts file with
 # columns totA totC totG totT as well as a .pos.gz file with all positions with data
 # to run: ./countReadsACGT.sh CA0906 thin1kb_common3
 
-BEE_ID=$1
-DIR_OUT="results/SNPs/"$2
+BEE_ID="${1}"
+DIR_OUT="results/SNPs/${2}"
 DIR_IN="../filtered_bams/results/"
-BAM="${DIR_IN}/${ID}.sort.dedup.baq.bam"
+BAM="${DIR_IN}/${BEE_ID}.sort.dedup.baq.bam"
 DIR_SINGLE_BAM_LIST="../bee_samples_listed/list_indiv_bams"
-REF="../data/honeybee_genome/honeybee_Amel_4.5"
+REF="../data/honeybee_genome/Amel_4.5_scaffolds.fa"
 
 # make output and scratch directories
 mkdir -p "${DIR_OUT}/countsACGT"
