c10a6a3e1f1df2d7b848e8db034f4d1078b3dc8e
ecalfee <erincalfee@gmail.com>
2020-03-02 17:49:43 -0800

    updated denominator pi. still need to make outlier fst plots

--- within_ancestry/plot_pi_fst_from_allele_freqs.R
+++ within_ancestry/plot_pi_fst_from_allele_freqs.R
@@ -18,10 +18,17 @@ gaps_tot <- read.table("../data/honeybee_genome/gaps.bed") %>%
   mutate(length = V3 - V2) %>%
   summarise(tot = sum(length)) %>%
   unlist(.)
-genome_size <- chr_length_tot - gaps_tot #236*10^6 # genome size
 n_snps <- 3510834 # from wc -l chr.var.sites
+# genome_size <- chr_length_tot - gaps_tot #236*10^6 # genome size
+# actually, use all sites passing quality filters for the denominator of pi:
+all_sites <- data.frame(
+  bin_name = read.table("../geno_lik_and_SNPs/results/1cM_bins.names", header = F, stringsAsFactors = F)$V1,
+  n_sites = read.table("../geno_lik_and_SNPs/results/1cM_bins_total_counts.txt",
+                        header = F, sep = "\t")$V2)
+genome_size <- sum(all_sites$n_sites)
 frac_snps <- n_snps/genome_size # multiplier for any snp-based heterozygosity estimate
 # fraction of the genome that is variable, i.e. snps
+
 
 # get data
 pops <- read.table("../bee_samples_listed/byPop/combined_sept19_pops.list",
@@ -92,9 +99,10 @@ ggsave("../../bee_manuscript/figures/pi_by_latitude_from_folded_SFS.png", device
 
 # load new data?
 load_new_data = F
+#load_new_data = T
 
 # read in 1/10th of the allele freq data:
-n_snp = 10
+#n_snp = 10
 
 # I sampled fraction n_snp of all SNPs, so the total fraction of the genome that are SNPs is:
 
