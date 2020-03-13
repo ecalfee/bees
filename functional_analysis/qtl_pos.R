fc55538edc9d2dbbe112c1637b7cfb9c44b9bd34
Erin Calfee <erincalfee@gmail.com>
2020-03-12 23:36:45 -0700

    spoetter qtl markers

--- functional_analysis/qtl_pos.R
+++ functional_analysis/qtl_pos.R
@@ -3,10 +3,12 @@ chr_lengths <- read.table("../data/honeybee_genome/chr.list", sep = "\t", string
 colnames(chr_lengths) <- c("scaffold", "length", "chr_group", "chr_lg")
 
 # spoetter qtls
-spoetter <- read.table("../data/Spoetter_2012_QTLs/spoetter_2012_qtl_markers.csv",
+spoetter <- read.table("../data/Spoetter_2012_QTLs/spoetter_2012_Amel3_start_end.csv",
                        sep = ",", header = T, stringsAsFactors = F) %>%
-  filter(marker_type %in% c("start", "end") &
-           QTL_study == "Spoetter 2012")
+  left_join(., rename(chr_lengths, chr = chr_group), by = "chr") %>%
+  mutate(QTL = 1:nrow(.)) %>%
+  dplyr::select(scaffold, start, end, QTL, chr_lg) %>%
+  rename(chr = scaffold, LG = chr_lg)
 # 44k snp chip
 snps_44k <- read.table("../data/Spoetter_2012_QTLs/SNP_Info_HoneyBeeSNPAssay.csv",
                        sep = ",", header = T, stringsAsFactors = F)
@@ -20,24 +22,6 @@ spoetter_snps %>%
   dplyr::select(QTL_study, QTL_name, marker_type, chr, pos, source, nearest_SNP, left.flanking.sequence, right.flanking.sequence, chr_HAv3.1, pos_HAv3.1_start, pos_HAv3.1_end) %>%
   write.table(., "results/Spoetter_2012_QTL_coordinates.txt",
               sep = "\t", col.names = T, row.names = F, quote = F)
-filter(snps_44k, linkage.group == "Group6") %>% 
-  arrange(Cooridnate) %>% tail() %>%
-  filter(Cooridnate == 14575555)
-filter(snps_44k, linkage.group == "Group16") %>% 
-  arrange(Cooridnate) %>% tail() %>%
-  filter(Cooridnate == 5626467)
-filter(snps_44k, linkage.group == "Group16") %>% 
-  arrange(Cooridnate) %>%
-  filter(Cooridnate %in% c(3194214, 3198573)) %>%
-  head()
-filter(snps_44k, linkage.group == "Group16") %>% 
-  arrange(Cooridnate) %>%
-  filter(Cooridnate < 3194214) %>%
-  tail()
-filter(snps_44k, linkage.group == "Group16") %>% 
-  arrange(Cooridnate) %>%
-  filter(Cooridnate > 3194214) %>%
-  head()
 snps_44k %>%
   arrange(linkage.group, Cooridnate) %>%
   write.table("../data/Spoetter_2012_QTLs/SNPs_44k.txt",
@@ -58,6 +42,33 @@ snps_44k_coord <- snps_44k %>%
   rename(Amel3_scaffold = scaffold) %>%
   dplyr::select(lsid, Amel3_scaffold, linkage.group, pos) %>%
   left_join(., Hav3.1_coord_44k, by = "lsid") %>%
-  filter(!is.na(start)) # filter out snps with no match
+  filter(!is.na(start)) %>% # filter out snps with no match
+  rename(chr = Amel3_scaffold,
+         start_HAv3.1 = start,
+         end_HAv3.1 = end,
+         scaffold_HAv3.1 = scaffold) #%>%
+  #mutate(start = pos - 1, 
+        # end = pos) %>%
+  #dplyr::select(chr, start, end, 
+   #             scaffold_HAv3.1, start_HAv3.1, end_HAv3.1,
+    #            perc_identical, lsid)
 # now which snps fall within qtls?
-head(spoetter)
+qtl_spoetter <- do.call(rbind,
+                        lapply(1:nrow(spoetter), function(i){
+  filter(snps_44k_coord, chr == spoetter$chr[i] &
+           pos >= spoetter$start[i] &
+           pos <= spoetter$end[i]) %>%
+                            mutate(qtl = spoetter$QTL[i])
+}))
+#qtl_spoetter <- bedr(
+#  engine = "bedtools", 
+ # input = list(a = snps_44k_coord,
+  #             b = spoetter), 
+#  method = "map", 
+# params = "-g ../data/honeybee_genome/chr.lengths -c 4 -o distinct",
+ # check.chr = F
+#) %>%
+ # data.table::setnames(c(colnames(snps_44k_coord), "qtl"))
+
+table(paste0(qtl_spoetter$qtl, qtl_spoetter$scaffold_HAv3.1))
+
