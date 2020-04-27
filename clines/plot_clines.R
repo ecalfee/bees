c8173f39e614979aed8bd4e40ca152e1a0a5858a
Erin Calfee <erincalfee@gmail.com>
2020-04-27 11:35:56 -0700

    output txt file of cline results to share with Turid/Webster

--- clines/plot_clines.R
+++ clines/plot_clines.R
@@ -2472,4 +2472,19 @@ summary(glm.fit)
 glm.fit2 = glm(alpha ~ abs_lat + enjambre, data = d_A, family = gaussian(link = "logit"))
 summary(glm.fit2)
 
+# make snp cline file for Webster group:
+load("../local_ancestry/results/sites_rpos.RData")
+sites_clines <- sites_rpos %>%
+  mutate(snp_index = 1:nrow(.)) %>%
+  left_join(., clines$params, by = "snp_index") %>%
+  dplyr::select(snp_index, scaffold, chr, chr_n, pos, major, minor, 
+cM_Mb, pos_cM, term, estimate, std.error, statistic, p.value, conf.low, conf.high)
 
+sites_clines %>%
+  pivot_wider(., id_cols = c("snp_index", "scaffold", "chr", "chr_n", "pos", "major", "minor", 
+                             "cM_Mb", "pos_cM", "term"),
+              names_from = "term", names_sep = ".",
+              values_from = c("estimate", "std.error", "p.value", "conf.low", "conf.high")) %>%
+  write.table("results/ind_snp_nls_multistart_clines/clines_4_Turid.txt",
+              col.names = T, row.names = F,
+              sep = "\t", quote = F)
